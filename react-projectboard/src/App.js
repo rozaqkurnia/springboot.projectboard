7ee12bc420821b0788ef6a5e7f7531d93969c358
Rozaq Kurnia <gazerockrozaq@gmail.com>
2021-01-13 21:16:00 +0700

    Create react app, install redux and other packages

---
+++ react-projectboard/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="alert alert-warning">Welcome to the project board</h1>
+      </div>
+    );
+  }
+}
+
+
+export default App;
