84d5145f8238b7f382342936a22e8db0d915530b
Rozaq Kurnia <gazerockrozaq@gmail.com>
2021-01-14 00:12:09 +0700

    Create Redux Store and Presist to DB Path

--- react-projectboard/src/components/ProjectTask/AddProjectTask.js
+++ react-projectboard/src/components/ProjectTask/AddProjectTask.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { addProjectTask } from '../../actions/projectTaskActions'
 
 class AddProjectTask extends Component {
     constructor(){
@@ -22,7 +25,8 @@ class AddProjectTask extends Component {
             acceptanceCriteria: this.state.acceptanceCriteria,
             status: this.state.status
         }
-        console.log(newProjectTask);
+        //console.log(newProjectTask);
+        this.props.addProjectTask(newProjectTask, this.props.history);
     }
     render() {
         return (
@@ -77,4 +81,13 @@ class AddProjectTask extends Component {
     }
 }
 
-export default AddProjectTask;
+AddProjectTask.propTypes = {
+    AddProjectTask: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, {addProjectTask}) (AddProjectTask);
