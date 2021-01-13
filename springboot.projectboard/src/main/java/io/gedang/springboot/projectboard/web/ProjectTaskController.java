8f53cd2c30591660cefe5f7b6cc0bbadf93f3fbe
Rozaq Kurnia <gazerockrozaq@gmail.com>
2021-01-13 19:11:33 +0700

    Add Read Operations

--- springboot.projectboard/src/main/java/io/gedang/springboot/projectboard/web/ProjectTaskController.java
+++ springboot.projectboard/src/main/java/io/gedang/springboot/projectboard/web/ProjectTaskController.java
@@ -35,4 +35,15 @@ public class ProjectTaskController {
         ProjectTask newPT = projectTaskService.saveOrUpdateProjectTask(projectTask);
         return new ResponseEntity<ProjectTask>(newPT, HttpStatus.CREATED);
     }
+
+    @GetMapping("/all")
+    public Iterable<ProjectTask> getAllPTs(){
+        return projectTaskService.findAll();
+    }
+
+    @GetMapping("/{pt_id}")
+    public ResponseEntity<?> getPTById(@PathVariable Long pt_id){
+        ProjectTask projectTask = projectTaskService.findById(pt_id);
+        return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
 }
