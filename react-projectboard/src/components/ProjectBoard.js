bc982125ab3b38e96e629c8014fd842a032e1a25
Rozaq Kurnia <gazerockrozaq@gmail.com>
2021-01-14 10:23:29 +0700

    display project task data

--- react-projectboard/src/components/ProjectBoard.js
+++ react-projectboard/src/components/ProjectBoard.js
@@ -9,9 +9,43 @@ class ProjectBoard extends Component {
     componentDidMount(){
         this.props.getBacklog();
     }
-    render() {
+    render() { 
+        const {project_tasks} = this.props.project_tasks
+        let BoardContent;
+        let todoItems = [];
+        let inProgressItems = [];
+        let doneItems = [];
+
+        const BoardAlgorithm = project_tasks => {
+            if(project_tasks.length < 1){
+                return (
+                    <div className="alert alert-info text-center" role="alert">
+                        No Project Task on this board
+                    </div>
+                );
+            } else {
+                const tasks = project_tasks.map(project_task => (
+                    <ProjectTaskItem key={project_task.id} project_task={project_task} />
+                ))
+
+                for(let i = 0; i < tasks.length; i++){
+                    if(tasks[i].props.project_task.status==="TO_DO"){
+                        todoItems.push(tasks[i])
+                    }
+
+                    if(tasks[i].props.project_task.status==="IN_PROGRESS"){
+                        inProgressItems.push(tasks[i])
+                    }
+
+                    if(tasks[i].props.project_task.status==="DONE"){
+                        doneItems.push(tasks[i])
+                    }
+                }
+            }
+        };
+
+        BoardAlgorithm(project_tasks);
         return (
-            
             <div className="container">
                 <Link to="/addProjectTask" className="btn btn-primary mb-3">
                     <i className="fas fa-plus-circle"> Create Project Task</i>
@@ -28,7 +62,7 @@ class ProjectBoard extends Component {
                                 </div>
                             </div>
 
-                            <ProjectTaskItem />
+                            {todoItems}
                             
                         </div>
                         <div className="col-md-4">
@@ -37,8 +71,7 @@ class ProjectBoard extends Component {
                                     <h3>In Progress</h3>
                                 </div>
                             </div>
-                            <ProjectTaskItem />
-
+                            {inProgressItems}
                             
                         </div>
                         <div className="col-md-4">
@@ -47,8 +80,7 @@ class ProjectBoard extends Component {
                                     <h3>Done</h3>
                                 </div>
                             </div>
-                            
-
+                            {doneItems}
                             
                         </div>
                     </div>
